493ae057f91831bad395bdc290af677c705f73f1
Kris Stoltz <83829350+Kris-Stoltz@users.noreply.github.com>
2021-10-20 05:14:41 +0200

    Update playlist.py

--- playlist.py
+++ playlist.py
@@ -7,8 +7,8 @@ class Playlist:
     def __init__(self):
         self.client = spotipy.Spotify(
             auth_manager=SpotifyOAuth(
-                client_id='b3dd4de2f85148a3bacc3229f33249b4',
-                client_secret='0d6310264413436799d48bec3f5ae8d5',
+                client_id='id',
+                client_secret='secret',
                 redirect_uri='http://example.com',
                 scope='playlist-modify-private',
                 username='4wtfllm7kh9lrwhu1nitlm49i',
